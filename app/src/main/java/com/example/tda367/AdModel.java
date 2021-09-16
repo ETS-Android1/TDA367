12615f392c8a6355ad978813294e8bf1a624fe8d
albertwickman <albert.wickman@hotmail.se>
2021-09-16 10:44:27 +0200

    Updated the ad class to a view instead of activity

--- app/src/main/java/com/example/tda367/AdModel.java
+++ app/src/main/java/com/example/tda367/AdModel.java
@@ -1,18 +1,29 @@
 package com.example.tda367;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.tda367.ui.dashboard.DashboardViewModel;
+import com.example.tda367.ui.home.HomeViewModel;
+import com.example.tda367.ui.notifications.NotificationsViewModel;
+
 import org.w3c.dom.Text;
 
-public class AdModel extends AppCompatActivity {
+public class AdModel extends Fragment {
+    private NotificationsViewModel notificationsViewModel;
     int price;
     String name;
 
@@ -21,14 +32,15 @@ public class AdModel extends AppCompatActivity {
 
     Button saveButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel = ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        editName = (EditText) findViewById(R.id.editName);
-        editPrice = (EditText) findViewById(R.id.editPrice);
-        saveButton = (Button) findViewById(R.id.saveButton);
+        editName = (EditText) view.findViewById(R.id.editName);
+        editPrice = (EditText) view.findViewById(R.id.editPrice);
+        saveButton = (Button) view.findViewById(R.id.saveButton);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +48,8 @@ public class AdModel extends AppCompatActivity {
                 name = editName.getText().toString();
                 price = Integer.valueOf(editPrice.getText().toString());
 
-                showToast("Your details have been updated!");
             }
         });
-    }
-
-    private void showToast(String text) {
-        Toast.makeText(AdModel.this, text, Toast.LENGTH_LONG).show();
+        return view;
     }
 }
