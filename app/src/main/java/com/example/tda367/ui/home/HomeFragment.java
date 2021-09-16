36c85b3e347a15eb17af23c09b179b5eef600813
albertwickman <albert.wickman@hotmail.se>
2021-09-16 13:59:47 +0200

    Fixed a bug that threw array out of bounds exeption when pressing the back button

--- app/src/main/java/com/example/tda367/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tda367/ui/home/HomeFragment.java
@@ -1,33 +1,17 @@
 package com.example.tda367.ui.home;
 
-import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
-
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.tda367.R;
-import com.example.tda367.ui.notifications.NotificationsViewModel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
@@ -36,10 +20,15 @@ public class HomeFragment extends Fragment {
     ImageButton nextButton;
     ImageButton backButton;
 
-    int currentImage = 0;
-    int numImages = 3;
+    int[] images = {
+            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_home_black_24dp,
+            R.drawable.ic_dashboard_black_24dp
+    };
 
-    int[] images = {R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background, R.drawable.ic_home_black_24dp};
+    int currentImage = 0;
+    int numImages = images.length;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -52,40 +41,42 @@ public class HomeFragment extends Fragment {
         nextButton = (ImageButton) view.findViewById(R.id.nextButton);
         backButton = (ImageButton) view.findViewById(R.id.backButton);
 
-        //Just set one Click listener for the image
         nextButton.setOnClickListener(changeNextImage);
         backButton.setOnClickListener(changePreviousImage);
+
+        myImageView.setImageResource(images[currentImage]);
+
         return view;
     }
 
-        View.OnClickListener changeNextImage = new OnClickListener() {
-
-            public void onClick(View v) {
-                //Increase Counter to move to next Image
+    View.OnClickListener changeNextImage = new OnClickListener() {
+        public void onClick(View v) {
+            if (currentImage == images.length) {
+                currentImage = 0;
+            } else {
+                //Increment Counter to move to next Image
                 currentImage++;
                 currentImage = currentImage % numImages;
-
                 //Set the image depending on the counter.
-                currentImage++;
                 currentImage = currentImage % images.length;
-
-                myImageView.setImageResource(images[currentImage]);
-            }
-        };
-
-    View.OnClickListener changePreviousImage = new OnClickListener() {
-
-        public void onClick(View v) {
-            //Increase Counter to move to next Image
-            currentImage--;
-            currentImage = currentImage % numImages;
-
-            //Set the image depending on the counter.
-            currentImage--;
-            currentImage = currentImage % images.length;
-
+        }
             myImageView.setImageResource(images[currentImage]);
         }
-
     };
+
+    View.OnClickListener changePreviousImage = new OnClickListener() {
+        public void onClick(View v) {
+            if (currentImage == 0) {
+                currentImage = images.length - 1;
+            } else {
+                //Increment Counter to move to next Image
+                currentImage--;
+                currentImage = currentImage % numImages;
+                //Set the image depending on the counter.
+                currentImage = currentImage % images.length;
+            }
+            myImageView.setImageResource(images[currentImage]);
+        }
+    };
+
 }
