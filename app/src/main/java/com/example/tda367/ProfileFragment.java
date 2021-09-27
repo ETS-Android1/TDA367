4631fcc975c62fe36bb700f4a41531c9a6c294da
albertwickman <albert.wickman@hotmail.se>
2021-09-27 20:42:22 +0200

    Clean up

--- app/src/main/java/com/example/tda367/ProfileFragment.java
+++ app/src/main/java/com/example/tda367/ProfileFragment.java
@@ -8,12 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -30,36 +26,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         firebaseAuth = FirebaseAuth.getInstance();
         View view = LayoutInflater.from(getContext()).inflate(R.layout.profile_user, container, false);
-        setUpRecyclerView(view);
-
         return view;
     }
 
-
     private void setAdapter(View view) {
         recyclerViewAdapter = new RecyclerViewAdapter(adList);
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewProfile);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recyclerView.setAdapter(recyclerViewAdapter);
     }
 
-    private void setUpRecyclerView(View view) {
-        firestore.collection("cars").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-
-                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                        adList.add(new CarAdModel(documentSnapshot.getString("name"), documentSnapshot.getString("area"), 200));
-
-                    }
-                    setAdapter(view);
-                } else {
-                    System.out.println("Error: " + task.getException());
-                }
-            }
-        });
-    }
 
 
 }
