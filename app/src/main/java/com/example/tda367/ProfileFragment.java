1b5286441ca6205c0bbc59002751ccb62bda3829
magnetixaft <Liseberg20!>
2021-09-28 13:44:42 +0200

    navigation from ProfileFragment to AddCarAdFragment

--- app/src/main/java/com/example/tda367/ProfileFragment.java
+++ app/src/main/java/com/example/tda367/ProfileFragment.java
@@ -3,11 +3,14 @@ package com.example.tda367;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -18,6 +21,7 @@ public class ProfileFragment extends Fragment {
     private RecyclerViewAdapter recyclerViewAdapter;
     private ProfileFragment profileFragment;
     private FirebaseAuth firebaseAuth;
+    private Button addCarAdButton;
 
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
     ArrayList<CarAdModel> adList = new ArrayList<>();
@@ -26,6 +30,14 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         firebaseAuth = FirebaseAuth.getInstance();
         View view = LayoutInflater.from(getContext()).inflate(R.layout.profile_user, container, false);
+
+        addCarAdButton = (Button) view.findViewById(R.id.addCarAdButton);
+        addCarAdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadAddCarAdFragment();
+            }
+        });
         return view;
     }
 
@@ -34,6 +46,12 @@ public class ProfileFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.recyclerViewProfile);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void loadAddCarAdFragment(){
+        Fragment addCarAdFragment = new AddCarAdFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, addCarAdFragment).commit();
     }
 
 
