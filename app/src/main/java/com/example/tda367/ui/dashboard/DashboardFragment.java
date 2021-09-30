d4fd6a7218ae190e45d8ddc7ae1abece743e1ece
Magnetixaft <jsjsandberg@gmail.com>
2021-09-30 23:54:18 +0200

    Updated CarAdModel with all attributes and updated getter from Firebase to populate recyclerView

--- app/src/main/java/com/example/tda367/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/tda367/ui/dashboard/DashboardFragment.java
@@ -1,38 +1,29 @@
 package com.example.tda367.ui.dashboard;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tda367.CarAdModel;
-import com.example.tda367.ProfileFragment;
 import com.example.tda367.RecyclerViewAdapter;
 import com.example.tda367.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class DashboardFragment extends Fragment {
 
@@ -57,16 +48,15 @@ public class DashboardFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recyclerView.setAdapter(recyclerViewAdapter);
     }
+    //Kanske ska köra Long istället för int
     private void setUpRecyclerView(View view){
         firestore.collection("cars").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-
                     for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                        //TODO ändra till int, documentSnapshot.getString("price")
-                        carList.add(new CarAdModel(documentSnapshot.getString("name"), documentSnapshot.getString("area"), 200));
-
+                        carList.add(new CarAdModel(documentSnapshot.getString("CarBrand"), documentSnapshot.getString("CarModel"), documentSnapshot.getString("CarTitle"), documentSnapshot.getString("CarYear"),
+                                documentSnapshot.getString("CarLocation"), documentSnapshot.getLong("CarPrice").intValue(), documentSnapshot.getLong("CarId").intValue()));//Kanske är CarID
                     }
                     setAdapter(view);
                 } else {
