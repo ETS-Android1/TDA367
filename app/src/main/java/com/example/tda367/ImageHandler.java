f2e841f641bc12f44ce50b2fbc79dc36c6e983f6
josefn-chalmers <josefn@student.chalmers.se>
2021-10-04 08:52:00 +0200

    Added image uploading method

--- app/src/main/java/com/example/tda367/ImageHandler.java
+++ app/src/main/java/com/example/tda367/ImageHandler.java
@@ -1,5 +1,12 @@
 package com.example.tda367;
 
+
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
@@ -7,24 +14,44 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.User;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class ImageHandler {
-    FirebaseStorage storage = FirebaseStorage.getInstance();
-    StorageReference storageRef = storage.getReference(); //root reference
-    StorageReference imagesRef = storageRef.child("images"); //image reference -> /images
-    //StorageReference carRef = imagesRef.child(); //carID reference -> /images/carID
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference(); //root reference
+    private StorageReference imagesRef = storageRef.child("images"); //image reference -> /images
+
+
+    private String getFirebasePath(String uid){
+        StorageReference carRef = imagesRef.child("2"); //add carid as parameter when ready
+        // carID image reference -> /images/carID/example.jpg
+        return carRef.getPath();
+    }
+
+    private String getLocalPath(){
+        // Write a function that gets a local path from an android phone.
+        return "path";
+    }
+
+    private void uploadPicture(){
+        Uri file = Uri.fromFile(new File(getLocalPath()));
+        StorageReference imageRef = imagesRef.child(file.getLastPathSegment());//Gets last part of the path (file name)
+        UploadTask uploadTask = imageRef.putFile(file);
+
+
+        //Observer for done/fail status.
+        uploadTask.addOnFailureListener(exception -> {
+
+        }).addOnSuccessListener(taskSnapshot -> {
+
+        });
 
 
 
-
-
-    private String getCurrentUid(){
-        return FirebaseAuth.getInstance().getUid();
     }
 
 
-    private void generateFolder(){
-
-    }
 
 }
