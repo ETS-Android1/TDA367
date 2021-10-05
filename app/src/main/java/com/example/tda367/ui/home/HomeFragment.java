693515b7fdc131e227547c9319dacf84356bcd9d
albertwickman <albert.wickman@hotmail.se>
2021-10-05 11:03:25 +0200

    Fixed navigation for the buttons except for the how did i do button

--- app/src/main/java/com/example/tda367/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tda367/ui/home/HomeFragment.java
@@ -4,21 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.view.View.OnClickListener;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.tda367.AddCarAdFragment;
+import com.example.tda367.ProfileFragment;
 import com.example.tda367.R;
+import com.example.tda367.SignUpPaymentFragment;
+import com.example.tda367.ui.dashboard.DashboardFragment;
+import com.example.tda367.ui.notifications.NotificationsFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
 
-    ImageView myImageView;
-    ImageButton nextButton;
-    ImageButton backButton;
+    private ImageView myImageView;
+    private ImageButton nextButton;
+    private ImageButton backButton;
+    private Button findCarButton;
+    private Button addCarButton;
+    private Button howDoButton;
+    private FirebaseAuth firebaseAuth;
 
     int[] images = {
             R.drawable.ic_launcher_foreground,
@@ -36,18 +48,65 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         myImageView = (ImageView) view.findViewById(R.id.myImageView);
         nextButton = (ImageButton) view.findViewById(R.id.nextButton);
         backButton = (ImageButton) view.findViewById(R.id.backButton);
+        findCarButton = (Button) view.findViewById(R.id.findCarButton);
+        addCarButton = (Button) view.findViewById(R.id.addCarButton);
+        howDoButton = (Button) view.findViewById(R.id.howDoButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         nextButton.setOnClickListener(changeNextImage);
         backButton.setOnClickListener(changePreviousImage);
+
+
+        findCarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDashboardFragment();
+            }
+        });
+
+        // if logged in, go to add car ad, else go to sign in
+        addCarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firebaseAuth.getCurrentUser() != null){
+                    loadAddCarFragment();
+                }
+                else {
+                    loadSignInFragment();
+                }
+            }
+        });
+
 
         myImageView.setImageResource(images[currentImage]);
 
         return view;
     }
+
+    private void loadAddCarFragment(){
+        Fragment addCarFragment = new AddCarAdFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, addCarFragment).commit();
+    }
+
+    private void loadDashboardFragment(){
+        Fragment dashboardFragment = new DashboardFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, dashboardFragment).commit();
+    }
+
+    private void loadSignInFragment(){
+        Fragment signInFragment = new NotificationsFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, signInFragment).commit();
+    }
+
 
     View.OnClickListener changeNextImage = new OnClickListener() {
         public void onClick(View v) {
