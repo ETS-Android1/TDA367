ab3df61609679577f7176700c2e44fc48d322789
Magnetixaft <jsjsandberg@gmail.com>
2021-10-20 17:07:08 +0200

    Test for Listing

--- app/src/test/java/edu/stulb/rentalcar/ModelTest.java
+++ app/src/test/java/edu/stulb/rentalcar/ModelTest.java
@@ -1,15 +1,21 @@
 package edu.stulb.rentalcar;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import edu.stulb.rentalcar.model.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stulb.rentalcar.model.Car;
+import edu.stulb.rentalcar.model.CarManufacturer;
+import edu.stulb.rentalcar.model.Card;
+import edu.stulb.rentalcar.model.Listing;
+import edu.stulb.rentalcar.model.Location;
+import edu.stulb.rentalcar.model.Reservation;
+import edu.stulb.rentalcar.model.User;
 
 public class ModelTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
     @Test
     public void carCreationIsCorrect(){
@@ -19,4 +25,26 @@ public class ModelTest {
         assertEquals(car.getCarYear(), 2021);
         assertEquals(car.getCarManufacturer().getManufacturer(), "Honda");
     }
+    @Test
+    public void locationIsCorrect(){
+        Location location = new Location("Göteborg");
+        assertEquals(location.getCity(), "Göteborg");
+    }
+    @Test
+    public void reservationIsCorrect() {
+        Reservation reservation = new Reservation();
+        List<Long> testList = new ArrayList<>();
+        assertEquals(reservation.getReservationsDatesList(), testList);
+    }
+    @Test
+    public void listingIsCorrect(){
+        CarManufacturer carManufacturer = new CarManufacturer("Volvo");
+        Car car = new Car("v90", carManufacturer, 2005);
+        Location location = new Location("Göteborg");
+        Card card = new Card("Hannes Thörn", "5355830012341234", "11/25", "111");
+        User user = new User("Hannes", "Hannes@gmail.com", card);
+        Listing listing = new Listing(car, 200, location, user);
+        assertEquals(listing.getCar().getCarModel(), "Volvo");
+    }
+
 }
