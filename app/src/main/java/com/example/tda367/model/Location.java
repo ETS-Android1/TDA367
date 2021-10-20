021f3f9b23e16d80d3d10e982e824a61bc5c1775
Magnetixaft <jsjsandberg@gmail.com>
2021-10-20 12:25:23 +0200

    Refactor of Card, Location and Reservation

--- app/src/main/java/com/example/tda367/model/Location.java
+++ app/src/main/java/com/example/tda367/model/Location.java
@@ -1,4 +1,24 @@
 package com.example.tda367.model;
 
+/**
+ * Location is a class to represent the location of a car
+ * @author Johan Sandberg
+ */
 public class Location {
+    private final String city;
+
+    /**
+     * Constructor to create a mew location
+     * @param city
+     */
+    public Location(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return String of name of city
+     */
+    public String getCity() {
+        return city;
+    }
 }
--- app/src/main/java/com/example/tda367/model/Reservation.java
+++ app/src/main/java/com/example/tda367/model/Reservation.java
@@ -1,4 +1,28 @@
 package com.example.tda367.model;
 
+import java.util.List;
+
+/**
+ * Reservation is a class to represent the dates the car is booked
+ * @author Johan Sandberg
+ */
 public class Reservation {
+    private List<Long> reservationsDatesList;
+
+    /**
+     * Constructor to create a list to store dates
+     * Uses Long to store dates @see <a href="https://docs.oracle.com/javase/8/docs/api/?java/util/Date.html">https://docs.oracle.com/javase/8/docs/api/?java/util/Date.html</a>
+     * @param reservationsDatesList
+     */
+    public Reservation(List<Long> reservationsDatesList) {
+        this.reservationsDatesList = reservationsDatesList;
+    }
+
+    /**
+     * Getter for reservationsDatesList
+     * @return a list of reservationsDatesList
+     */
+    public List<Long> getReservationsDatesList() {
+        return reservationsDatesList;
+    }
 }
