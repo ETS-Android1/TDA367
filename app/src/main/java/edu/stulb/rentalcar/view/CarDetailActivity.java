dc909a9bebf921513d4fba8e0d5bcbac39de01d1
albertwickman <albert.wickman@hotmail.se>
2021-10-22 09:49:05 +0200

    Changed activity to fragment

--- app/src/main/java/edu/stulb/rentalcar/view/CarDetailActivity.java
+++ app/src/main/java/edu/stulb/rentalcar/view/CarDetailActivity.java
@@ -1,6 +1,10 @@
 package edu.stulb.rentalcar.view;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -9,11 +13,12 @@ import edu.stulb.rentalcar.model.CarAdModel;
 import com.example.tda367.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
-public class CarDetailActivity extends AppCompatActivity {
+public class CarDetailActivity extends Fragment {
 
     CarAdModel carAdModel;
     EditText carName, carBrand, carModel, carYear, carPrice, carLocation;
@@ -21,29 +26,31 @@ public class CarDetailActivity extends AppCompatActivity {
 
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_detail);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_car_detail, container, false);
 
-        carAdModel = (CarAdModel) getIntent().getSerializableExtra("car");
-        carName = (EditText) findViewById(R.id.carName);
-        carBrand = (EditText) findViewById(R.id.carBrand);
-        carModel = (EditText) findViewById(R.id.carModel);
-        carYear = (EditText) findViewById(R.id.carYear);
-        carPrice = (EditText) findViewById(R.id.carPrice);
-        carLocation = (EditText) findViewById(R.id.carLocation);
-        imageView = (ImageView) findViewById(R.id.imageView);
+            // TODO change serializable
+            //carAdModel = (CarAdModel) getIntent().getSerializableExtra("car");
 
-        carName.setText(carAdModel.getCarTitle());
-        carBrand.setText(carAdModel.getCarBrand());
-        carModel.setText(carAdModel.getCarModel());
-        carYear.setText(carAdModel.getCarYear());
-        //carPrice.setText(carAdModel.getCarPrice());
-        carLocation.setText(carAdModel.getCarLocation());
+            carName = (EditText) view.findViewById(R.id.carName);
+            carBrand = (EditText) view.findViewById(R.id.carBrand);
+            carModel = (EditText) view.findViewById(R.id.carModel);
+            carYear = (EditText) view.findViewById(R.id.carYear);
+            carPrice = (EditText) view.findViewById(R.id.carPrice);
+            carLocation = (EditText) view.findViewById(R.id.carLocation);
+            imageView = (ImageView) view.findViewById(R.id.imageView);
+
+            carName.setText(carAdModel.getCarTitle());
+            carBrand.setText(carAdModel.getCarBrand());
+            carModel.setText(carAdModel.getCarModel());
+            carYear.setText(carAdModel.getCarYear());
+            //carPrice.setText(carAdModel.getCarPrice());
+            carLocation.setText(carAdModel.getCarLocation());
 
 
-        firestore = FirebaseFirestore.getInstance();
-    }
+            firestore = FirebaseFirestore.getInstance();
 
+            return view;
+        }
 }
