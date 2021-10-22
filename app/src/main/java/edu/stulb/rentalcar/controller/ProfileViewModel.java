a2daa4680177da817a2e5d48ab91d2f79e59c4a8
Magnetixaft <jsjsandberg@gmail.com>
2021-10-22 14:02:40 +0200

    Fixed Profile to get users listings in Recyclerview to match refactored code

--- app/src/main/java/edu/stulb/rentalcar/controller/ProfileViewModel.java
+++ app/src/main/java/edu/stulb/rentalcar/controller/ProfileViewModel.java
@@ -1,63 +1,57 @@
 package edu.stulb.rentalcar.controller;
 
-import android.net.Uri;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.tda367.R;
-import edu.stulb.rentalcar.model.CarAdModel;
-import edu.stulb.rentalcar.model.FirebaseHandler;
-import edu.stulb.rentalcar.view.AddCarAdFragment;
-import edu.stulb.rentalcar.view.SignInFragment;
 
 import java.util.ArrayList;
 
+import edu.stulb.rentalcar.model.listing.Listing;
+import edu.stulb.rentalcar.model.listing.ListingHandler;
+import edu.stulb.rentalcar.model.user.UserHandler;
+import edu.stulb.rentalcar.view.AddCarAdFragment;
+import edu.stulb.rentalcar.view.SignInFragment;
+
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-    private FirebaseHandler firebaseHandler = new FirebaseHandler();
+    UserHandler userHandler = UserHandler.getInstance();
+    ListingHandler listingHandler = ListingHandler.getInstance();
 
     public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public ArrayList<Listing> getUsersListings(){
+        return listingHandler.getUserListings(userHandler.getCurrentUser().getEmail());
     }
 
-    public void addAd(String carTitle, String carBrand, String carModel, String carYear, int carPrice, String carLocation, Uri selectedImage){
-        firebaseHandler.addAd(carTitle, carBrand, carModel, carYear, carPrice, carLocation, selectedImage);
-    }
+    /*
+        public void addAd(String carTitle, String carBrand, String carModel, String carYear, int carPrice, String carLocation, Uri selectedImage){
+            firebaseHandler.addAd(carTitle, carBrand, carModel, carYear, carPrice, carLocation, selectedImage);
+        }
 
-    public String getUserEmail(){
-        return firebaseHandler.getCurrentUser().getEmail().toString();
-    }
+        public String getUserEmail(){
+            return firebaseHandler.getCurrentUser().getEmail().toString();
+        }
 
-    public void loadSignInFragment(FragmentManager fragmentManager){
+
+     */
+    public void loadSignInFragment(FragmentManager fragmentManager) {
         Fragment signInFragment = new SignInFragment();
         fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, signInFragment).commit();
     }
 
-    public void loadAddCarAdFragment(FragmentManager fragmentManager){
+    public void loadAddCarAdFragment(FragmentManager fragmentManager) {
         Fragment addCarAdFragment = new AddCarAdFragment();
         fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, addCarAdFragment).commit();
     }
-
+/*
     public void signOut(){
         firebaseHandler.signOut();
     }
 
-    public ArrayList<CarAdModel> carAds(){
-        ArrayList<CarAdModel> cars = new ArrayList<CarAdModel>();
-
-
-
-        return cars;
-    }
+ */
 
 }
--- app/src/main/java/edu/stulb/rentalcar/view/ProfileFragment.java
+++ app/src/main/java/edu/stulb/rentalcar/view/ProfileFragment.java
@@ -12,15 +12,10 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import edu.stulb.rentalcar.controller.ProfileViewModel;
-import edu.stulb.rentalcar.model.CarAdModel;
 import com.example.tda367.R;
+
 import edu.stulb.rentalcar.RecyclerViewAdapter;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import java.util.ArrayList;
+import edu.stulb.rentalcar.controller.ProfileViewModel;
 
 
 public class ProfileFragment extends Fragment {
@@ -32,15 +27,10 @@ public class ProfileFragment extends Fragment {
     private Button logOutButton;
     private String userEmail;
 
-    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-    ArrayList<CarAdModel> adList = new ArrayList<>();
-
-
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.profile_user, container, false);
-        setUpRecyclerView(view);
 
-        userEmail = profileViewModel.getUserEmail();
+        //userEmail = profileViewModel.getUserEmail();
         addCarAdButton = (Button) view.findViewById(R.id.addCarAdButton);
         addCarAdButton.setOnClickListener(v -> profileViewModel.loadAddCarAdFragment(getParentFragmentManager()));
 
@@ -49,35 +39,19 @@ public class ProfileFragment extends Fragment {
             signOut();
             makeToast("You have been logged out!");
         });
+        setAdapter(view);
         return view;
     }
 
     private void setAdapter(View view) {
-        recyclerViewAdapter = new RecyclerViewAdapter(adList, getContext());
+        recyclerViewAdapter = new RecyclerViewAdapter(profileViewModel.getUsersListings(), getContext());
         RecyclerView recyclerView = view.findViewById(R.id.recyclerViewProfile);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recyclerView.setAdapter(recyclerViewAdapter);
     }
 
-    private void setUpRecyclerView(View view){
-        firestore.collection("cars").get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                    if (documentSnapshot.getString("CarEmail").equals(userEmail)){
-                        adList.add(new CarAdModel(documentSnapshot.getString("CarBrand"), documentSnapshot.getString("CarModel"), documentSnapshot.getString("CarTitle"), documentSnapshot.getString("CarYear"),
-                                documentSnapshot.getString("CarLocation"), documentSnapshot.getLong("CarPrice").intValue(), documentSnapshot.getString("CarId"), documentSnapshot.getString("CarEmail"), (ArrayList<Long>) documentSnapshot.get("CarBookedDates")));//Kanske är CarID
-                    }
-
-                }
-                setAdapter(view);
-            } else {
-                System.out.println("Error: " + task.getException());
-            }
-        });
-    }
-
     private void signOut(){
-        profileViewModel.signOut();
+        //profileViewModel.signOut();
         profileViewModel.loadSignInFragment(getParentFragmentManager());
     }
 
