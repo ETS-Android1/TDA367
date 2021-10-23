ee2b653bba8fe6d88a64d65b9058eb36f645440a
hannemanne <71085652+hannemanne@users.noreply.github.com>
2021-10-23 16:04:49 +0200

    Confirmation after bookning finished

--- app/src/main/java/edu/stulb/rentalcar/controller/BookingConfirmationViewModel.java
+++ app/src/main/java/edu/stulb/rentalcar/controller/BookingConfirmationViewModel.java
@@ -1,6 +1,45 @@
 package edu.stulb.rentalcar.controller;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModel;
 
+import com.example.tda367.R;
+
+import edu.stulb.rentalcar.model.listing.Listing;
+import edu.stulb.rentalcar.model.listing.ListingHandler;
+import edu.stulb.rentalcar.view.HomeFragment;
+
 public class  BookingConfirmationViewModel extends ViewModel {
+    ListingHandler listingHandler = ListingHandler.getInstance();
+    String daysBooked;
+    Listing bookedListing;
+
+    public void getBookedListing(String listingId, String listingBookedDays){
+        daysBooked = listingBookedDays;
+        bookedListing = listingHandler.getListingFromUid(listingId);
+    }
+    public String displayBookedDates(){
+        return daysBooked;
+    }
+
+    public String displayCarTitle(){
+        return bookedListing.getCar().getCarManufacturer().getManufacturer()+" "+
+                bookedListing.getCar().getCarModel();
+    }
+
+    public String displayPricePerDay(){
+        return String.valueOf(bookedListing.getPricePerDay());
+    }
+
+    public String displayTotalPrice(int numDaysBooked){
+        System.out.println(numDaysBooked);
+        int totalPrice = bookedListing.getPricePerDay()*numDaysBooked;
+        return String.valueOf(totalPrice);
+    }
+    public void loadHomeFragment(FragmentManager fragmentManager){
+        Fragment homeFragment = new HomeFragment();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, homeFragment).commit();
+    }
+
 }
