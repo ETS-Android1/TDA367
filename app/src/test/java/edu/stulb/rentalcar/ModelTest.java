dcf6148199567b9f0a11bcbe429312bb886760cc
Magnetixaft <jsjsandberg@gmail.com>
2021-10-24 21:10:35 +0200

    Listing test

--- app/src/test/java/edu/stulb/rentalcar/ModelTest.java
+++ app/src/test/java/edu/stulb/rentalcar/ModelTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import edu.stulb.rentalcar.model.listing.Car;
 import edu.stulb.rentalcar.model.listing.CarManufacturer;
+import edu.stulb.rentalcar.model.listing.DateHandler;
 import edu.stulb.rentalcar.model.listing.Listing;
 import edu.stulb.rentalcar.model.listing.ListingHandler;
 import edu.stulb.rentalcar.model.listing.Location;
@@ -45,14 +47,34 @@ public class ModelTest {
         Car car = new Car("v90", carManufacturer, "2005");
         Location location = new Location("Göteborg");
         Reservation reservation = new Reservation();
+        ArrayList<Long> reservationList = new ArrayList<>();
+        Reservation reservation1 = new Reservation(reservationList);
         Card card = new Card("Hannes Thörn", "5355830012341234", "11/25", "111");
         User user = new User("Hannes", "Hannes@gmail.com","Stulb123", card);
         Listing listing = new Listing(car, 200, location, user.getEmail(), reservation, "PathentillBilden");
+        Listing listing1 = new Listing(car, 200, location, user.getEmail(), reservation, "PathentillBilden", "idTillListing");
+        HashMap<String, Object> listingHashMap= listing.toHashMap();
+
+        HashMap<String, Object> hashMapToTest = new HashMap<>();
+
+        hashMapToTest.put("ListingId", listing.getUid());
+        hashMapToTest.put("CarManufacturer", listing.getCar().getCarManufacturer().getManufacturer());
+        hashMapToTest.put("CarModel", listing.getCar().getCarModel());
+        hashMapToTest.put("CarYear", listing.getCar().getCarYear());
+        hashMapToTest.put("ListingPricePerDay", listing.getPricePerDay());
+        hashMapToTest.put("ListingLocation", listing.getLocation().getCity());
+        hashMapToTest.put("UserEmail", listing.getUserEmail());
+        hashMapToTest.put("ReservationBookedDates", listing.getReservation().getReservationsDatesList());
+        hashMapToTest.put("ImagePath", listing.getImagePath());
+
         assertEquals(listing.getCar().getCarModel(), "v90");
         assertEquals(listing.getLocation().getCity(), "Göteborg");
+        assertEquals(listing.getReservation().getReservationsDatesList(), new ArrayList<>());
+        assertEquals(listing.getImagePath(), "PathentillBilden");
         assertEquals(listing.getPricePerDay(), 200);
         assertEquals(listing.getUserEmail(), "Hannes@gmail.com");
         assertEquals(listing.getUid(), listing.getUid());
+        assertEquals(listingHashMap, hashMapToTest);
     }
 
     @Test
@@ -69,10 +91,23 @@ public class ModelTest {
         Reservation reservation = new Reservation();
         ListingHandler.getInstance().createListing(car, 200, location, userEmail, reservation, "PathTillBilden");
         ArrayList<Listing> tempList = ListingHandler.getInstance().getListings();
-        System.out.println(tempList.get(0).getCar().getCarModel());
-        System.out.println();
     }
 
+    @Test
+    public void dateHandlerIsCorrect(){
+        DateHandler dateHandler = new DateHandler();
+        Long dateLong = dateHandler.convertToMillis("2020/10/24");
+        System.out.println(dateLong);
+        assertEquals(dateLong, dateLong);
 
+        ArrayList<Long> dateList= new ArrayList<>();
+        dateList.add(dateLong);
+        String datesString = dateHandler.getClickedDatesString(dateList);
+        System.out.println(datesString);
+        assertEquals(datesString, "2020/10/24, ");
+
+        String dateAsString = dateHandler.formatDate(2020,9,24);
+        assertEquals(dateAsString, "2020/10/24");
+    }
 
 }
