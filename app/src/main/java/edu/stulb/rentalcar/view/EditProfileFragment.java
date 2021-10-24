3f7ca63d31fbdea313f89a17c20969b0a6637fac
Magnetixaft <jsjsandberg@gmail.com>
2021-10-24 14:07:59 +0200

    Added EditProfile, does nothing

---
+++ app/src/main/java/edu/stulb/rentalcar/view/EditProfileFragment.java
@@ -0,0 +1,22 @@
+package edu.stulb.rentalcar.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tda367.R;
+
+public class EditProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.edit_profile, container, false);
+
+        return view;
+    }
+}
